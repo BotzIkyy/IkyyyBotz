06e10d5d91a7afe83653faa1e49f3c05926d6b33
MeMansッ <80680923+ZackMans@users.noreply.github.com>
2022-04-15 22:51:08 +0700

    Add files via upload

--- zackm.js
+++ zackm.js
@@ -153,6 +153,36 @@ case 'menu': {
 mans.sendMessage(from, {text:listmn}, {quoted:m})
 }
 break
+case 'inspect': {
+let linkRegex = /chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+        let [, code] = args.join(" ").match(linkRegex) || {}
+        if (!code) return m.reply("Link Invalid")
+        let res = await mans.groupQueryInvite(code)
+        if (!res) return m.reply(String(res))
+        let teks = `
+    「 Group Link Inspector 」
+⬡ *ID :* ${res.id}
+⬡ *Subject :* ${res.subject}
+⬡ *Subject Update By :* @${res.subjectOwner.split("@")[0]}
+⬡ *Subject Update At :* ${moment(res.subjectTime * 1000).tz("Asia/Jakarta").format("DD-MM-YYYY, HH:MM:SS")}
+⬡ *Creator :* @${res.owner ? res.owner.split("@")[0] : res.id.split("-")[0]}
+⬡ *Create At :* ${moment(res.creation * 1000).tz("Asia/Jakarta").format("DD-MM-YYYY, HH:MM:SS")}
+⬡ *Members Length :* ${res.size}
+⬡ *Desc Update By :* ${res.descOwner ? "@" + res.descOwner.split("@")[0] : ""}
+⬡ *Desc Update At :* ${moment(res.descTime * 1000).tz("Asia/Jakarta").format("DD-MM-YYYY, HH:MM:SS")}
+⬡ *Desc ID :* ${res.descId}
+⬡ *Description :*\n${res.desc ? res.desc : "No Description"}
+⬡ *Friends Who Are Known to Join :*\n${res.participants ? res.participants.map((user, i) => ++i + ". @" + user.jid.split("@")[0]).join("\n").trim() : "Not Found"}
+        `
+        let pp
+        try {
+            pp = await mans.profilePictureUrl(res.id, "image")
+        } catch {
+            pp = "https://tse2.mm.bing.net/th?id=OIP.n1C1oxOvYLLyDIavrBFoNQHaHa&pid=Api&P=0&w=153&h=153"
+        }
+        mans.sendFile(m.from, pp, "", m, { caption: teks, mentions: await mans.parseMention(teks) })
+}
+break
 case 'join': {
 if (!args[0]) return m.reply("Linknya mana kak?")
 vdd = args[0]
@@ -170,7 +200,7 @@ if (sizny < 50) {
 m.reply(`Maaf anggota group anda kurang dari 50, minimal agar bot join harus mempunyai lebih dari 50 anggota`)
 } else if (siznya > 50) {
 let ddg = args[0].split("https://chat.whatsapp.com/")[1]
-await mans.groupAcceptInvite(ddg).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+await mans.groupAcceptInvite(ddg)
 m.reply("Succes")
 } else {
 m.reply("Error")
